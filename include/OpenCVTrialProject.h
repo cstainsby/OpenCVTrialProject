8fccccbe6f7b2bdd2fb06b88367df8d5f96cd01a
cstainsby <cstainsby@zagmail.gonzaga.edu>
2021-10-05 21:47:11 -0700

    initial commit, cmake is grabbing opencv libraries and binaries correctly

---
+++ include/OpenCVTrialProject.h
@@ -0,0 +1,11 @@
+﻿// OpenCVTrialProject.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+#include <C:\Program Files (x86)\Libraries\OpenCV\build\include\opencv2\imgcodecs.hpp>
+#include <C:\Program Files (x86)\Libraries\OpenCV\build\include\opencv2\highgui.hpp>
+#include <C:\Program Files (x86)\Libraries\OpenCV\build\include\opencv2\imgproc.hpp>
---
+++ src/OpenCVTrialProject.cpp
@@ -0,0 +1,12 @@
+﻿// OpenCVTrialProject.cpp : Defines the entry point for the application.
+//
+
+#include "OpenCVTrialProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
